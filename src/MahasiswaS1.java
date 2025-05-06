6707ee629da12493c24163c6d945b2ea3479fefb
Orang9 <egbert.fewi@gmail.com>
2025-05-06 21:18:58 +0700

    Adding class Mahasiswa S1

---
+++ src/MahasiswaS1.java
@@ -0,0 +1,21 @@
+public class MahasiswaS1 extends Mahasiswa {
+    String jalurMasuk;
+    String dosenPembimbing;
+
+    public String getJalurMasuk() {
+        return jalurMasuk;
+    }
+
+    public void setJalurMasuk(String jalurMasuk) {
+        this.jalurMasuk = jalurMasuk;
+    }
+
+    public String getDosenPembimbing() {
+        return dosenPembimbing;
+    }
+
+    public void setDosenPembimbing(String dosenPembimbing) {
+        this.dosenPembimbing = dosenPembimbing;
+    }
+
+}
