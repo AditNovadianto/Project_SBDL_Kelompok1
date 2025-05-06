c50e50cd1b1f9b735b8246c8c9ab6c9e59623142
Orang9 <egbert.fewi@gmail.com>
2025-05-06 21:18:13 +0700

    Adding class Mata Kuliah

---
+++ src/Matakuliah.java
@@ -0,0 +1,30 @@
+
+public class Matakuliah {
+    String kode;
+    String nama;
+    int sks;
+
+    public String getKode() {
+        return kode;
+    }
+
+    public void setKode(String kode) {
+        this.kode = kode;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public int getSks() {
+        return sks;
+    }
+
+    public void setSks(int sks) {
+        this.sks = sks;
+    }
+}
