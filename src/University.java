cbc2293f692ff5e5a34127f7fd323e44ca2a0390
Orang9 <egbert.fewi@gmail.com>
2025-05-06 21:13:43 +0700

    Adding class University

---
+++ src/University.java
@@ -0,0 +1,21 @@
+public class University {
+
+    String name;
+    String address;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
