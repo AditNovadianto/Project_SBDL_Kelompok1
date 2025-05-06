46b934c7f4980110edf59ef473ee889b4932159c
Orang9 <egbert.fewi@gmail.com>
2025-05-06 21:17:06 +0700

    Adding class KRS

---
+++ src/KRS.java
@@ -0,0 +1,23 @@
+
+public class KRS {
+    int totalSKS;
+    int semester;
+    String tahunKurikulum;
+    Matakuliah matakuliah;
+
+    public void setTotalSKS(int totalSKS) {
+        this.totalSKS = totalSKS;
+    }
+
+    public void setSemester(int semester) {
+        this.semester = semester;
+    }
+
+    public void setTahunKurikulum(String tahunKurikulum) {
+        this.tahunKurikulum = tahunKurikulum;
+    }
+
+    public void setMatakuliah(Matakuliah matakuliah) {
+        this.matakuliah = matakuliah;
+    }
+}
