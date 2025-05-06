9e95814689f69e6d37dc14e832fdfcf33e2fe319
Orang9 <egbert.fewi@gmail.com>
2025-05-06 21:19:06 +0700

    Adding class Mahasiswa S2

---
+++ src/MahasiswaS2.java
@@ -0,0 +1,30 @@
+public class MahasiswaS2 extends Mahasiswa {
+    String jalurMasuk;
+    String dosenPembimbing;
+    String asalUniv;
+
+    public String getJalurMasuk() {
+        return jalurMasuk;
+    }
+
+    public void setJalurMasuk(String jalurMasuk) {
+        this.jalurMasuk = jalurMasuk;
+    }
+
+    public String getDosenPembimbing() {
+        return dosenPembimbing;
+    }
+
+    public void setDosenPembimbing(String dosenPembimbing) {
+        this.dosenPembimbing = dosenPembimbing;
+    }
+
+    public String getAsalUniv() {
+        return asalUniv;
+    }
+
+    public void setAsalUniv(String asalUniv) {
+        this.asalUniv = asalUniv;
+    }
+    
+}
