930b331426eda6108f578365eb9b9ed97274762b
Orang9 <egbert.fewi@gmail.com>
2025-05-07 21:17:55 +0700

    Change file structure and add Query by Example

---
+++ src/Apps/ProgramStudi.java
@@ -0,0 +1,40 @@
+package Apps;
+import java.util.List;
+import java.util.ArrayList;
+
+public class ProgramStudi {
+    private String name;
+    private String code;
+    private List<Mahasiswa> listMahasiswa;
+
+    // Constructor
+    public ProgramStudi(String name, String code) {
+        this.name = name;
+        this.code = code;
+        this.listMahasiswa = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public List<Mahasiswa> getListMahasiswa() {
+        return listMahasiswa;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public void addMahasiswa(MahasiswaS1 newMahasiswaS1) {
+        listMahasiswa.add(newMahasiswaS1);
+    }
+}
---
+++ src/Query/QBESelectByNIM.java
@@ -0,0 +1,26 @@
+package Query;
+import com.db4o.Db4oEmbedded;
+import com.db4o.ObjectContainer;
+import com.db4o.ObjectSet;
+
+import Apps.Mahasiswa;
+
+public class QBESelectByNIM {
+    public static void main(String[] args) {
+        // Buka database (akan buat file db jika belum ada)
+        ObjectContainer db = Db4oEmbedded.openFile("dataSBDL.db4o");
+
+        // Query By Example
+        Mahasiswa template = new Mahasiswa();
+        template.setNim("1232001011");
+        ObjectSet<Mahasiswa> result = db.queryByExample(template);
+
+        System.out.println("Hasil QBE:");
+        while (result.hasNext()) {
+            Mahasiswa m = (Mahasiswa) result.next();
+            System.out.println(m.getNim() + " - " + m.getName() + " - " + m.getBeasiswa());
+        }
+
+        db.close();
+    }
+}
